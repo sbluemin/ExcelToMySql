7d07a1b192832501ef02420cce5516e03ae02cc7
Hyeonmin Yoon <yoonhyeonmin@gmail.com>
2017-01-14 19:25:35 +0900

    Add UnitTest project but empty test case

---
+++ Test/MySql/SqlTableTests.cs
@@ -0,0 +1,28 @@
+﻿using ExcelToMySql.MySql;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text;
+
+namespace ExcelToMySql.MySql.Tests
+{
+    [TestClass()]
+    public class SqlTableTests
+    {
+        [TestMethod()]
+        public void NewQuery_DropTableTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void NewQuery_CreateTableTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void NewQuery_AddDatasTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
