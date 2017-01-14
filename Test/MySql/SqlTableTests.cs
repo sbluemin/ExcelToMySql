fa43140be1073039f4a6564a11d9d582c2457dea
Hyeonmin Yoon <yoonhyeonmin@gmail.com>
2017-01-14 19:39:50 +0900

    Add DropTableTest case

--- Test/MySql/SqlTableTests.cs
+++ Test/MySql/SqlTableTests.cs
@@ -1,28 +1,64 @@
 ﻿using ExcelToMySql.MySql;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Text;
+using ExcelToMySql.Excel;
+using System.Collections.Generic;
 
 namespace ExcelToMySql.MySql.Tests
 {
     [TestClass()]
     public class SqlTableTests
     {
+        const string _tableName = "SqlTableTest";
+
         [TestMethod()]
         public void NewQuery_DropTableTest()
         {
-            Assert.Fail();
+            var table = new SqlTable(new ExcelMetaData(), new SqlTableConfiguration
+            {
+                TableName = "SqlTableTest"
+            });
+
+            var builder = new StringBuilder();
+            table.NewQuery_DropTable(builder);
+
+            Assert.AreEqual("DROP TABLE IF EXISTS `SqlTableTest`;\n", builder.ToString());
         }
 
         [TestMethod()]
         public void NewQuery_CreateTableTest()
         {
-            Assert.Fail();
+            var config = new SqlTableConfiguration
+            {
+                TableName = "SqlTableTest"
+            };
+
+            var metaData = new ExcelMetaData();
+            metaData.ColumnNames.Add("text_column_name_1");
+            metaData.ColumnNames.Add("int_column_name_2");
+
+            Assert.Fail("Not used.");
         }
 
         [TestMethod()]
         public void NewQuery_AddDatasTest()
         {
-            Assert.Fail();
+            var config = new SqlTableConfiguration
+            {
+                TableName = "SqlTableTest"
+            };
+
+            var metaData = new ExcelMetaData();
+            metaData.ColumnNames.Add("text_column_name_1");
+            metaData.ColumnNames.Add("int_column_name_2");
+            metaData.Datas.Add(new List<object>
+            {
+                "test_field",
+                100
+            });
+
+
+            Assert.Fail("Not used.");
         }
     }
 }
