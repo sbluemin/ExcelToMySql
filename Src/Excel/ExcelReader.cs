1127ecdb23ab05703dee01070f2be3efc8349de1
Hyeonmin Yoon <yoonhyeonmin@gmail.com>
2017-01-14 19:58:53 +0900

    Add customize entry point feature

--- Src/Excel/ExcelReader.cs
+++ Src/Excel/ExcelReader.cs
@@ -18,15 +18,16 @@ namespace ExcelToMySql.Excel
         /// </summary>
         /// <param name="reader"></param>
         /// <param name="metaData"></param>
+        /// <param name="config"></param>
         /// <returns></returns>
-        private static bool ReadColumnName(IExcelDataReader reader, ExcelMetaData metaData)
+        private static bool ReadColumnName(IExcelDataReader reader, ExcelMetaData metaData, ExcelReaderConfiguration config)
         {
             if (!reader.Read())
             {
                 return false;
             }
 
-            for (int i = 0; i < reader.FieldCount; i++)
+            for (int i = config.DataEntryPointColumnIndex; i < reader.FieldCount; i++)
             {
                 metaData.ColumnNames.Add(reader.GetString(i));
             }
@@ -40,44 +41,55 @@ namespace ExcelToMySql.Excel
         /// <param name="absoluteFilePath"></param>
         /// <param name="outMetaData"></param>
         /// <returns></returns>
-        public static bool ReadExcel(string absoluteFilePath, out ExcelMetaData outMetaData)
+        public static void ReadExcel(string absoluteFilePath, out ExcelMetaData outMetaData)
+        {
+            var config = new ExcelReaderConfiguration();
+            ReadExcel(absoluteFilePath, config, out outMetaData);
+        }
+
+        /// <summary>
+        /// Read .xlsx file and convert to ExcelMetaData.
+        /// </summary>
+        /// <param name="absoluteFilePath"></param>
+        /// <param name="config"></param>
+        /// <param name="outMetaData"></param>
+        /// <returns></returns>
+        public static void ReadExcel(string absoluteFilePath, ExcelReaderConfiguration config, out ExcelMetaData outMetaData)
         {
             outMetaData = new ExcelMetaData();
 
-            try
+            using (var stream = File.Open(absoluteFilePath, FileMode.Open, FileAccess.Read))
             {
-                using (var stream = File.Open(absoluteFilePath, FileMode.Open, FileAccess.Read))
+                using (var excelReader = ExcelReaderFactory.CreateOpenXmlReader(stream))
                 {
-                    using (var excelReader = ExcelReaderFactory.CreateOpenXmlReader(stream))
+                    // Move data entry point.
+                    for (var i = 0; i < config.DataEntryPointRowIndex; i++)
                     {
-                        // 컬럼 정보를 읽음
-                        excelReader.IsFirstRowAsColumnNames = true;
-                        if (!ReadColumnName(excelReader, outMetaData))
+                        if (!excelReader.Read())
                         {
                             throw new Exception("Read failed.");
                         }
+                    }
 
-                        // 이후 데이터 읽기
-                        while (excelReader.Read())
+                    // Read column info
+                    if (!ReadColumnName(excelReader, outMetaData, config))
+                    {
+                        throw new Exception("Read failed.");
+                    }
+
+                    // Read datas
+                    while (excelReader.Read())
+                    {
+                        var row = new List<object>();
+                        for (int i = config.DataEntryPointColumnIndex; i < excelReader.FieldCount; i++)
                         {
-                            // 컬럼 정보는 0부터 있고 데이터는 그 뒤에 있다.
-                            var row = new List<object>();
-                            for (int i = 0; i < excelReader.FieldCount; i++)
-                            {
-                                row.Add(excelReader.GetValue(i));
-                            }
-
-                            outMetaData.Datas.Add(row);
+                            row.Add(excelReader.GetValue(i));
                         }
+
+                        outMetaData.Datas.Add(row);
                     }
                 }
             }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-            }
-
-            return true;
         }
     }
 }
---
+++ Src/Excel/ExcelReaderConfiguration.cs
@@ -0,0 +1,8 @@
+﻿namespace ExcelToMySql.Excel
+{
+    public class ExcelReaderConfiguration
+    {
+        public int DataEntryPointColumnIndex = 0;
+        public int DataEntryPointRowIndex = 0;
+    }
+}
