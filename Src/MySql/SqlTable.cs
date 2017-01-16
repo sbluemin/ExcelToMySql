9971e6c850e62afc8631d5d609b8693ba3fb7a32
Hyeonmin Yoon <yoonhyeonmin@gmail.com>
2017-01-16 21:41:30 +0900

    Fix crash when read empty column

--- Src/MySql/SqlTable.cs
+++ Src/MySql/SqlTable.cs
@@ -36,12 +36,15 @@ namespace ExcelToMySql.MySql
 
             foreach (var i in MetaData.ColumnNames)
             {
-                foreach(var j in Configuration.SqlTypeMap)
+                if(i != null)
                 {
-                    if (i.Contains(j.Key))
+                    foreach (var j in Configuration.SqlTypeMap)
                     {
-                        builder.AppendFormat("`{0}` {1} NOT NULL,\n", i, j.Value);
-                        break;
+                        if (i.Contains(j.Key))
+                        {
+                            builder.AppendFormat("`{0}` {1} NOT NULL,\n", i, j.Value);
+                            break;
+                        }
                     }
                 }
             }
